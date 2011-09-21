8db89e2e115c86aa3ba9fcecf82d77a574652dcb
Hugh Winkler <hugh.winkler@wellstorm.com>
2011-09-20 19:41:15 -0500

    Refactor witsmlreplay to use the common code in post.rb.

--- witsmlreplay/witsmlreplay.rb
+++ witsmlreplay/witsmlreplay.rb
@@ -1,11 +1,8 @@
 require 'optparse'
-require 'net/http'
-require 'net/https'
-require 'uri'
 require 'rexml/document'
 require 'rexml/streamlistener'
 require 'time'
-require 'stringio'
+require 'post'
 
 $options = {}
 
@@ -33,30 +30,6 @@ opts =OptionParser.new do |o|
 end
 
 
-def post(io, url, user, pass)    
-  url = URI.parse(url)  if url.is_a? String
-  io = StringIO.new(io) if io.is_a? String
-
-  req = Net::HTTP::Post.new(url.path)
-  req.basic_auth user, pass if user && user.length > 0
-  req.body_stream = io
-  req.content_type = 'application/soap+xml'
-  #req.content_length = io.stat.size
-  req.content_length = io.size   # specific to StringIO class ? why no stat on that class?
-  http = Net::HTTP.new(url.host, url.port)  
-  http.use_ssl = true
-  http.read_timeout = 60 # secs
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  res = http.start {|http2| http2.request(req) }
-
-  case res
-  when Net::HTTPSuccess, Net::HTTPRedirection
-    # OK
-    res
-  else
-    res.error!
-  end
-end
 
 class MyListener
   include REXML::StreamListener
