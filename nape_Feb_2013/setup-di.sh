e0a68a2e8f2c2f3bd72c058fff518cc9db8794f0
Paul Teasdale <paul.teasdale@wellstorm.com>
2013-01-31 09:52:16 -0500

    configure a setup-di.sh with properties targeted to di.wellstorm.com

--- nape_Feb_2013/setup-di.sh
+++ nape_Feb_2013/setup-di.sh
@@ -1,7 +1,30 @@
+#export SIM_BASE_URL="https://artemis:8443/witsml/repository"
+#export SIM_WITSML_STORE_URL=https://artemis:8443/witsml/services/store
 export SIM_BASE_URL="https://di.wellstorm.com:443/witsml/repository"
+export SIM_WITSML_STORE_URL="https://di.wellstorm.com:443/witsml/services/store"
+
+#
+# UIDS
+#
 export SIM_UID_WELL="LIVE-42-177-32094-00-00"
+export SIM_UID_TIME_LOG="LIVE-L-499007-Time"
+export SIM_UID_DEPTH_LOG="LIVE-711f5bef-Depth"
+#
+# OBJECT NAMES
+#
 export SIM_NAME_WELL="LIVE KOENNING UNIT"
+export SIM_NAME_TIME_LOG="LIVE-TIMELOG"
+export SIM_NAME_DEPTH_LOG="LIVE-DEPTHLOG"
+#
+# FILE NAMES
+#
 export SIM_WELL_FILE=./data/well.witsml
 export SIM_WELLBORE_FILE=./data/wellbore.witsml
+export SIM_TIME_LOG_FILE=./data/time-log.witsml
+export SIM_DEPTH_LOG_FILE=./data/depth-log.witsml
+#
+# CREDENTIALS
+#
 export SIM_USERNAME=admin
+#export SIM_PASSWORD=admin
 export SIM_PASSWORD=quateraf
